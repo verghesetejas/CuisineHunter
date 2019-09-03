403991ef03c75e721bf43dac0c9f33a39493d99a
verghesetejas <verghesetejas@gmail.com>
2019-09-03 23:33:48 +0530

    Added new layout and buttons for links

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -12,7 +12,7 @@ export class RestaurantService {
   constructor( private httpClient: HttpClient ) { }
 
   public getQuery(params: any): Observable<any> {
-    const headers = new HttpHeaders().set('content-type', 'application/json').set('user-key', '49a4eb933ed5dcadf897cc4be97788bf');
-    return this.httpClient.get(`https://developers.zomato.com/api/v2.1/search?q=` + params, { headers: headers });
+    const headers = new HttpHeaders().set('content-type', 'application/json').set('user-key', Constants.USER_KEY);
+    return this.httpClient.get(`${Constants.ZOMATO_SEARCH_URL}` + params, { headers: headers });
   }
 }
