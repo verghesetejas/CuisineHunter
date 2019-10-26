42cf68c66aafe307996b3170f68e2821127cb7c6
verghesetejas <verghesetejas@gmail.com>
2019-10-27 01:33:11 +0530

    full features done

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,42 +10,7 @@ import { Auth } from '../models/auth.model';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  @ViewChild('footerComp', { static: false }) footer: ElementRef;
-  @ViewChild('userDP', { static: false }) userDP: ElementRef;
-  user: Auth;
-  title = "Cuisine Hunter";
-  sticky: number;
-
-  constructor( private authService: AuthenticationService,
-    private renderer: Renderer2,
-    private domSanitizer: DomSanitizer) { }
-
-  ngOnInit() {
-    window.addEventListener('scroll', this.scroll, true);
-  }
-  ngAfterViewInit(): void {
-    this.sticky = this.footer.nativeElement.getBoundingClientRect().top;
-  }
-
-  ngOnChanges(): void {}
-
-  /**
-   * Handles window scroll animations
-   */
-  @HostListener('window:scroll', ['$event'])
-  scroll(): void {
-    if (this.renderer && this.footer.nativeElement) {
-      if (window.pageYOffset > this.sticky) {
-        this.renderer.addClass(this.footer.nativeElement, 'sticky');
-      } else {
-        this.renderer.removeClass(this.footer.nativeElement, 'sticky');
-      }
-    }
-  };
-
-  ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.scroll, true);
-  }
+  ngOnInit() {}
 }
 
 
